aa3fde51426963a46de534f7f642ddb231017279
Santiago Cattaneo <santiago@rd-its.com>
2022-03-17 16:09:13 -0300

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,11 @@
+using BlazorPlugin2.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+var httpClient = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
+builder.Services.AddScoped(sp => httpClient);
+await builder.Build().RunAsync();
