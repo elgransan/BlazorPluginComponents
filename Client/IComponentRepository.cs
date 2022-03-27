4f967b49faff42092aecbb45af64042686082978
Santiago Cattaneo <santiago@rd-its.com>
2022-03-27 19:41:24 -0300

    * ModuleManager first version * Uploader incomplete

---
+++ Client/IComponentRepository.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorPlugin2.Client
+{
+    public interface IComponentRepository
+    {
+        bool CheckLoaded(string component);
+        Task GetList();
+        bool LoadComponent(string name);
+        Task UploadComponent(IBrowserFile nugetPackage);
+    }
+}
---
+++ Server/Controllers/ModuleManagerController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Reflection;
+
+namespace BlazorPlugin2.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ModuleManagerController : Controller
+    {
+        private readonly IWebHostEnvironment env;
+
+        public ModuleManagerController(IWebHostEnvironment env)
+        {
+            this.env = env;
+        }
+
+        [HttpPost]
+        public async Task<CreatedResult> Index([FromForm] IFormFile file)
+        {
+            // Open Zip file
+
+            // Read file by file
+
+            // Save all files in corresponding folder
+
+            using MemoryStream ms = new();
+            await file.OpenReadStream().CopyToAsync(ms);
+            var bytes = ms.ToArray();
+            var assembly = Assembly.Load(bytes);
+            var folderName = assembly.GetName().Name ?? file.FileName;
+
+            string path = Path.Combine(env.WebRootPath, folderName);
+
+            Directory.CreateDirectory(path);
+
+
+
+            await using FileStream fs = new(path, FileMode.Create);
+            await file.CopyToAsync(fs);
+
+            return new CreatedResult(path, untrustedFileName);
+        }
+    }
+}
